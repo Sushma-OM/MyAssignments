1c05869eb792b5d6b23522569167b7a3e95ad07d
Sushma-OM <126969115+Sushma-OM@users.noreply.github.com>
2023-03-11 00:30:12 +0530

    Add files via upload

--- week1/day1/IsPrime.java
+++ week1/day1/IsPrime.java
@@ -5,15 +5,18 @@ public class IsPrime {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		for (int i = 2; i<=20; i++) {
-		if (i%2==0)	
-			System.out.println(i+" Is Prime");
-		else
-			System.out.println(i+" Not prime");
+		int num=13;
+		boolean prime=true;
+		for (int i = 2; i<num; i++) {
+		if (num%i==0)	{
+			System.out.println(num+" Is Not Prime");
+		prime=false;
+		break;
 		}
-
+		}
+		 if(prime==true) {
+		 System.out.println(num+" Is  Prime");
+		 }
 	}
 
 }
